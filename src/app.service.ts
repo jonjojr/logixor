e275f246abe1fe24bad7f4a461d08eb2d294c4ac
Jon <jarzola@gmail.com>
2020-07-12 16:40:49 -0400

    added updated design

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'welcome to junimation. soon, i\'ll be able to tell you a whole lot more. stick around for project "journey". "You can use logic to justify almost anything. That\'s its power. And its flaw." - Capt. Janeway';
-  }
-}
